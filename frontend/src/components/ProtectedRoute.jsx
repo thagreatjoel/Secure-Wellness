db22b81572c4929d858998002ee49ae8b220b8a6
thagreatjoel <joeljonah06@gmail.com>
2025-08-19 13:31:30 +0530

    g

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,6 +0,0 @@
-import { Navigate, Outlet } from "react-router-dom";
-
-export default function ProtectedRoute() {
-  const isAuth = !!localStorage.getItem("token"); // Simple check, replace with context/auth
-  return isAuth ? <Outlet /> : <Navigate to="/login" />;
-}
